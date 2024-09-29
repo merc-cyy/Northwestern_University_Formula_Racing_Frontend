c8dda3f548d7c62fc5769e5b821f8838fe074f94
rpbeam04 <rpbeam04@gmail.com>
2024-09-29 11:34:05 -0500

    Exe Setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but they might need fine-tuning.
+build_exe_options = {
+    "excludes": ["tkinter", "numpy"],
+    "zip_include_packages": ["matplotlib"],
+}
+
+setup(
+    name="formula",
+    version="0.1",
+    description="My GUI application!",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("formula.py", base="gui")],
+)
