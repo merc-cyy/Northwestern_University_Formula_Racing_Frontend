d4c4d4158b39218221193f93e836c57dc640559b
rpbeam04 <rpbeam04@gmail.com>
2024-11-09 14:58:18 -0600

    main

---
+++ main.py
@@ -0,0 +1,17 @@
+### NFR DAQ Interface Main Program ###
+
+"""
+App Features
+
+- architecture
+  - matplotlib (graphs) and customtkinter (GUI)
+- custom windows
+  - how to accomplish? Anthony
+- displaying graphs
+- data storage
+- live data (feedback says we will wait on this)
+
+"""
+
+if __name__ == "__main__":
+    print('Hello DAQ')
