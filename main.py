703b568ed481ad3643fe39a8a9c149fd84557422
rpbeam04 <rpbeam04@gmail.com>
2024-11-17 13:47:05 -0600

    Hello app

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 ### NFR DAQ Interface Main Program ###
+import customtkinter as ctk
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 """
 App Features
@@ -13,5 +17,27 @@ App Features
 
 """
 
+class DAQInterface:
+    def __init__(self) -> None:
+        ctk.set_appearance_mode("dark")
+        self.root = ctk.CTk()
+        self.root.geometry("800x500")
+        self.root.title("NFR DAQ Interface")
+        self.root.update()
+        self.button = ctk.CTkButton(master = self.root,
+                                    text="Welcome to DAQ",
+                                    width=300,
+                                    height=50,
+                                    command=self.test_func)
+        self.button.place(relx=0.5,rely=0.5)
+        self.root.mainloop()
+
+    def test_func(self) -> None:
+        self.text = ctk.CTkTextbox(master = self.root,
+                                   width=300,
+                                   height=50
+                                   )
+        self.root.update()
+
 if __name__ == "__main__":
-    print('Hello DAQ')
+    app = DAQInterface()
