af49a943c22d3dc4229f37f431f1f0764049fbf6
merc-cyy <mercymuiruri2027@u.northwestern.edu>
2025-01-15 19:15:56 -0600

    some changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import customtkinter as ctk
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from test import MultiFrameApp
 
 """
 App Features
@@ -26,7 +27,7 @@ class DAQInterface:
         self.root.update()
         self.button = ctk.CTkButton(master = self.root,
                                     text="Welcome to DAQ",
-                                    command=self.test_func)
+                                    command=self.create_graph_app)
         self.button.place(relx=0.5,rely=0.5, anchor="center", relwidth=0.15, relheight = 0.1)
         self.root.mainloop()
 
@@ -34,6 +35,15 @@ class DAQInterface:
         self.text = ctk.CTkTextbox(master = self.root)
         self.text.place(relx = 0.5, rely = 0.8, anchor="center", relwidth = 0.7, relheight= 0.4)
         self.root.update()
+
+    def create_graph_app(self):
+        # Create a new window for GraphApp
+        graph_window = ctk.CTkToplevel(self)
+        graph_window.title("Graph Application")
+        graph_window.geometry("800x600")
+        
+        # Instantiate GraphApp in the new window
+        graph_app = MultiFrameApp()
 
 if __name__ == "__main__":
     app = DAQInterface()
